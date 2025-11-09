f0dd83ae8c90c25561298deae7c34384731a3389
Vadim Belov <bvdcode@users.noreply.github.com>
2025-11-08 17:58:25 -0800

    refactor: restructure routing in AppLayout for improved readability and organization

--- src/package/src/components/AppLayout.tsx
+++ src/package/src/components/AppLayout.tsx
@@ -7,25 +7,51 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 const AppLayout: FunctionComponent<ReactKitProps> = (props) => {
   return (
     <BrowserRouter basename={props.basename}>
-      <Box display="flex" width="100%" height="100%" flexDirection="column">
-        <NavigationBar {...props} />
+      <Routes>
+        <Route
+          element={
+            <Box
+              display="flex"
+              width="100%"
+              height="100%"
+              flexDirection="column"
+            >
+              <NavigationBar {...props} />
+              <Box component="main" sx={{ flex: 1, overflow: "auto", p: 3 }}>
+                <Routes>
+                  {props.pages.map((page) => {
+                    const element = page.protected ? (
+                      <ProtectedContent>{page.component}</ProtectedContent>
+                    ) : (
+                      page.component
+                    );
+                    return (
+                      <Route
+                        key={page.route}
+                        path={page.route}
+                        element={element}
+                      />
+                    );
+                  })}
+                </Routes>
+              </Box>
+            </Box>
+          }
+        >
+          {props.pages.map((page) => {
+            const element = page.protected ? (
+              <ProtectedContent>{page.component}</ProtectedContent>
+            ) : (
+              page.component
+            );
+            return (
+              <Route key={page.route} path={page.route} element={element} />
+            );
+          })}
+        </Route>
 
-        <Box component="main" sx={{ flex: 1, overflow: "auto", p: 3 }}>
-          <Routes>
-            {props.pages.map((page) => {
-              const element = page.protected ? (
-                <ProtectedContent>{page.component}</ProtectedContent>
-              ) : (
-                page.component
-              );
-              return (
-                <Route key={page.route} path={page.route} element={element} />
-              );
-            })}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Box>
-      </Box>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
   );
 };
