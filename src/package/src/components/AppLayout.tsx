4cfd310eed216cf029f36582bb9e15a301c166e4
Vadim Belov <bvdcode@users.noreply.github.com>
2025-11-23 18:30:51 -0800

    fix: set height to 100% for content container in AppLayout

--- src/package/src/components/AppLayout.tsx
+++ src/package/src/components/AppLayout.tsx
@@ -55,6 +55,7 @@ const AppLayout: FunctionComponent<ReactKitProps> = (props) => {
                       maxWidth: props.contentMaxWidth ?? "100%",
                       margin: "0 auto",
                       width: "100%",
+                      height: "100%",
                     }}
                   >
                     <Outlet />
